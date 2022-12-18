a1badc5e9bfb2153fcb4d4fda8087d7c47482bdb
DongHyun Jo <mr8356@konkuk.ac.kr>
2022-12-18 09:07:54 +0000

    12865

---
+++ python/Gold_12865.py
@@ -0,0 +1,20 @@
+#Knapsack
+import sys
+input = sys.stdin.readline
+n, k = map(int, input().split())
+
+stuff = [[0,0]]
+DP = [[0] * (k+1) for _ in range(n+1)]
+
+for _ in range(n):
+    stuff.append(list(map(int, input().split())))
+
+for i in range(1,n+1):
+    for j in range(1,k+1):
+        w,v = stuff[i]
+        if j >= w:
+            DP[i][j] = max(DP[i-1][j] , DP[i-1][j-w] + v)
+        else:
+            DP[i][j] = DP[i-1][j]
+
+print(DP[i][k])
