c158280a6b5bc37872cf7983bac14a642d136a96
DongHyun Jo <mr8356@konkuk.ac.kr>
2023-09-13 10:48:54 +0000

    complete 1918 후위

--- python/Gold_1918.py
+++ python/Gold_1918.py
@@ -2,15 +2,16 @@
 inp = list(input())
 stack = []
 result = []
-pri = {'*': 4, '/': 3, '+': 2, '-': 1,'(':0, ')':0}
+pri = {'*': 4, '/': 4, '+': 2, '-': 2, '(': 0, ')': 0}
 
 
 def removeBracket():
     global stack
-    for i in range(-1, -1*len(stack), -1):
-        if stack[i] == '(':
-            del stack[i]
-
+    while len(stack)>0:
+        ch = stack.pop()
+        if ch=='(':
+            return
+        result.append(ch)
 
 for i in range(len(inp)):
     ch = inp[i]
@@ -18,19 +19,17 @@ for i in range(len(inp)):
         result.append(ch)
     elif ch == ')':
         removeBracket()
-    elif ch=='(':
+    elif ch == '(':
         stack.append(ch)
     else:
-        if len(stack) > 0:
-            if pri[ch] > pri[stack[-1]]:
-                stack.append(ch)
-            else:
-                # 기존보다 우선순위 낮은얘오면 기존의 것을 차례대로 내보냄
-                while len(stack) > 0 and pri[ch] < pri[stack[-1]]:
-                    result.append(stack.pop())
-        else:
-            stack.append(ch)
+        if len(stack) > 0 and pri[ch] <= pri[stack[-1]]:
+            # 기존보다 우선순위 낮은얘오면 기존의 것을 차례대로 내보냄
+            while len(stack) > 0 and pri[ch] <= pri[stack[-1]]:
+                result.append(stack.pop())
+        stack.append(ch)
 while len(stack) > 0:
     result.append(stack.pop())
 
 print(''.join(result))
+# print(result)
+# print(stack)
